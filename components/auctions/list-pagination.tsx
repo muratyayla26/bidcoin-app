cc00798ced26989893b5a262d5bd23fefce0b1ca
muratyayla <murat.yayla.26@gmail.com>
2024-09-17 22:16:15 +0300

    auctions list page, pagination loading state improvement

--- components/auctions/list-pagination.tsx
+++ components/auctions/list-pagination.tsx
@@ -1,22 +1,35 @@
 "use client";
 
-import useScrollToTop from "@/lib/hooks/scrollToTop";
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import { Pagination } from "@nextui-org/pagination";
-import { useRouter } from "next/navigation";
+import useScrollToTop from "@/lib/hooks/scrollToTop";
 
 export const ListPagination = ({ page }: { page: string | undefined }) => {
   const router = useRouter();
+  const searchParams = useSearchParams();
   const initialPage = page ? Number(page) : 1;
+  const [isMounted, setIsMounted] = useState(false);
+  const [isRouting, setIsRouting] = useState(false);
 
   useScrollToTop("auctions-container");
 
+  useEffect(() => {
+    setIsMounted(true);
+    if (isMounted) {
+      setIsRouting(false);
+    }
+  }, [searchParams]);
+
   const handlePageChange = (event: number) => {
+    setIsRouting(true);
     router.push(`/auctions?page=${event}`);
   };
 
   return (
     <div className="my-6">
       <Pagination
+        isDisabled={isRouting}
         showControls
         total={10}
         initialPage={initialPage}
